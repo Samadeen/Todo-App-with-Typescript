d64199e4bc7e85538ca3dd544b03f9119ae1f8cf
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-07-19 07:26:51 +0100

    Created a Todo Form

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,15 @@
+const NewTodo = () => {
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor='text'>Todo Text</label>
+      <input type='text' id='text' />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
